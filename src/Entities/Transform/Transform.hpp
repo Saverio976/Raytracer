020a28e08af0f47d87a81905d35e7e3f5ddd29ff
bahmez <younes1.bahri@epitech.eu>
2023-04-26 18:50:30 +0200

    [Feat] (Transform) : add Transform.cpp/hpp

---
+++ src/Entities/Transform/Transform.hpp
@@ -0,0 +1,69 @@
+/*
+** EPITECH PROJECT, 2023
+** Raytracer
+** File description:
+** Transform.hpp
+*/
+
+#ifndef TRANSFORM_HPP_
+    #define TRANSFORM_HPP_
+
+    #include "ITransform.hpp"
+    #include "Vector3f.hpp"
+
+namespace RayTracer::Entities::Transform {
+    /**
+     * @brief The Transform class
+     *
+     * class that represent a transform (position, rotation, scale, ..) of
+     * an `IEntity`
+     */
+    class Transform : public ITransform {
+        public:
+            /**
+             * @brief Set the position
+             *
+             * @param position the position
+             */
+            void setPosition(const Vector3f &position);
+            /**
+             * @brief Get the position
+             *
+             * @return the position
+             */
+            const Vector3f &getPosition() const;
+            /**
+             * @brief Set the rotation
+             *
+             * @param rotation the rotation
+             */
+            void setRotation(const Vector3f &rotation);
+            /**
+             * @brief Get the rotation
+             *
+             * @return the rotation
+             */
+            const Vector3f &getRotation() const;
+            /**
+             * @brief Set the scale
+             *
+             * @param scale the scale
+             */
+            void setScale(const Vector3f &scale);
+            /**
+             * @brief Get the scale
+             *
+             * @return the scale
+             */
+            const Vector3f &getScale() const;
+
+            protected:
+            private:
+                Vector3f _position;
+                Vector3f _rotation;
+                Vector3f _scale;
+
+    };
+}
+
+#endif /*TRANSFORM_HPP_*/
