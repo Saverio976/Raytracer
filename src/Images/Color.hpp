19c119ffb4958c646eb2d7afaf77d8f1db7090b8
bahmez <younes1.bahri@epitech.eu>
2023-04-27 10:59:41 +0200

    [Refactor] (Color) : edit Color.hpp

--- src/Images/Color.hpp
+++ src/Images/Color.hpp
@@ -118,4 +118,14 @@ namespace RayTracer::Images {
     };
 }
 
+ostream& operator<<(ostream& os, const RayTracer::Images::Color& color) {
+    float a = color[RayTracer::Images::Color::Types::ALPHA];
+    float r = a * color[RayTracer::Images::Color::Types::RED] / 255;
+    float g = a * color[RayTracer::Images::Color::Types::GREEN] / 255;
+    float b = a * color[RayTracer::Images::Color::Types::BLUE] / 255;
+
+    os << r << ' ' << g << ' ' << b;
+    return os;
+}
+
 #endif /*COLOR_HPP_*/
