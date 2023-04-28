894d4846b391f967b386ad81cb593395f432200c
SimonVermeulen <simon.vermeulen@epitech.eu>
2023-04-29 00:25:07 +0200

    [Refactor] (Scenes): updated the test

--- tests/WrapperTest.cpp
+++ tests/WrapperTest.cpp
@@ -1,31 +1,33 @@
-/*
-** EPITECH PROJECT, 2023
-** Raytracer
-** File description:
-** WrapperTest.cpp
-*/
-
 #include <iostream>
 #include <libconfig.h++>
 #include "ConfigWrapper.hpp"
 
 using namespace Raytracer::Scenes;
 /*
- * Exemple d'un code récupérant les informations concernant la résolution dans le fichier testScene.config
+ * Exemple d'un code récupérant les informations concernant les sphères dans le fichier testScene.config
  */
-int main(int ac, char **av)
-{
+int main(int ac, char **av) {
     ConfigWrapper test;
+    char keys[5] = "xyzr";
+    char color_keys[4] = "rgb";
 
     test.readFile("./testScene.config");
-    std::shared_ptr<SceneConfig> sceneConfig = test.getScene();
 
-    std::cout << sceneConfig->getSetting("camera.resolution") << std::endl;
+    std::shared_ptr<SettingWrapper> setting = test.getScene();
 
-    for (int i = 0; i < sceneConfig->getLength(); i++) {
+    //récupération de l'objet contenant les sphères
+    setting->getSetting("primitives.spheres");
+    //parsing des deux sphères
+    for (int i = 0; i < setting->getLength(); i++) {
+        //récupération coordonnées :
+        std::cout << "sphere[" << i << "]" << std::endl << "\tcoords: " << std::endl << "\t\t";
+        for (int j = 0; j < 4; j++)
+            std::cout << keys[j] << " = " << static_cast<int>((*setting)[i][j]) << "\t";
 
-        int s = sceneConfig->getSettingValue<int>(i);
-        std::cout << i << " == " << s << std::endl;
+        //récupération couleurs :
+        std::cout << std::endl << "\tcolor: " << std::endl << "\t\t";
+        for (int k = 0; k < 3; k++)
+            std::cout << color_keys[k] << " = " << static_cast<int>((*setting)[i]["color"][k]) << "\t";
+        std::cout << std::endl;
     }
-
 }
