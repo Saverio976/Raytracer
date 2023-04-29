e5fe905706644207764c97d93067fa81ffe6ae5f
Xavier Mitault <xavier.mitault@epitech.eu>
2023-04-29 12:56:53 +0200

    [Feat] (Sphere/PlainMaterial) : first final implementation of a material

--- PluginsExt/Sphere/PlainMaterial.hpp
+++ PluginsExt/Sphere/PlainMaterial.hpp
@@ -8,6 +8,8 @@
 #ifndef PLAINMATERIAL_HPP_
     #define PLAINMATERIAL_HPP_
 
+    #include "Ray.hpp"
+    #include "Color.hpp"
     #include "ITransform.hpp"
     #include "Vector3f.hpp"
     #include "IMaterial.hpp"
@@ -17,8 +19,15 @@ namespace RayTracer::PluginsExt::Sphere {
         public:
             PlainMaterial(const IConfig &config);
 
-            const Images::Color &getColor(const Entities::Transform::ITransform &centerObj, const Entities::Transform::Vector3f &intersect, const Scenes::Displayable &displayable) const final;
+            Images::Color getColor(const Images::Ray &ray, const Entities::Transform::ITransform &centerObj, const Entities::Transform::Vector3f &intersect, const Scenes::Displayable &displayable) const final;
             void setColor(const Images::Color &color) final;
+
+            Images::Color round(const Images::Color &color) const;
+        private:
+            Images::Color _ambient;
+            Images::Color _diffuse;
+            Images::Color _specular;
+            float _shininess;
     };
 }
 
