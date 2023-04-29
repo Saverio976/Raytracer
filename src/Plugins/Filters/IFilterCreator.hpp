d1f2cf9265f7649ca1dc1a72bbecd92e4d600d2b
SimonVermeulen <simon.vermeulen@epitech.eu>
2023-04-29 19:39:19 +0200

    [Refactor] (Plugins): updated from IConfig to ISetting

--- src/Plugins/Filters/IFilterCreator.hpp
+++ src/Plugins/Filters/IFilterCreator.hpp
@@ -7,11 +7,9 @@
 
 #ifndef IFILTERCREATOR_HPP_
     #define IFILTERCREATOR_HPP_
-
     #include <memory>
     #include "IFilter.hpp"
-
-class IConfig; // TODO: create an iconfig please
+    #include "ISetting.hpp"
 
 namespace RayTracer::Plugins::Filters {
     /**
@@ -23,13 +21,13 @@ namespace RayTracer::Plugins::Filters {
         public:
             virtual ~IFilterCreator() = default;
             /**
-             * @brief Create a filter with a config
+             * @brief Create a filter with a setting
              *
-             * @param config the config
+             * @param config the setting
              *
              * @return the filter
              */
-            virtual std::unique_ptr<RayTracer::Filters::IFilter> create(const IConfig &config) = 0;
+            virtual std::unique_ptr<RayTracer::Filters::IFilter> create(const Scenes::ISetting &setting) = 0;
 
         protected:
         private:
