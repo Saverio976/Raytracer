925b4b8fe606312f36c8f61af55914a9a3f4287c
Xavier Mitault <xavier.mitault@epitech.eu>
2023-04-29 11:24:19 +0200

    [Fix] (Sphere/PlainMaterial) : corrct prototype

---
+++ PluginsExt/Sphere/PlainMaterial.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2023
+** Raytracer
+** File description:
+** PlainMaterial.cpp
+*/
+
+#include "PlainMaterial.hpp"
+
+namespace RayTracer::PluginsExt::Sphere {
+    PlainMaterial::PlainMaterial(const IConfig &config) {}
+
+    const Images::Color &PlainMaterial::getColor(const Entities::Transform::ITransform &centerObj, const Entities::Transform::Vector3f &intersect, const Scenes::Displayable &displayable) const
+    {
+    }
+}
--- PluginsExt/Sphere/PlainMaterial.hpp
+++ PluginsExt/Sphere/PlainMaterial.hpp
@@ -8,6 +8,8 @@
 #ifndef PLAINMATERIAL_HPP_
     #define PLAINMATERIAL_HPP_
 
+    #include "ITransform.hpp"
+    #include "Vector3f.hpp"
     #include "IMaterial.hpp"
 
 namespace RayTracer::PluginsExt::Sphere {
@@ -15,7 +17,7 @@ namespace RayTracer::PluginsExt::Sphere {
         public:
             PlainMaterial(const IConfig &config);
 
-            const Images::Color &getColor() const final;
+            const Images::Color &getColor(const Entities::Transform::ITransform &centerObj, const Entities::Transform::Vector3f &intersect, const Scenes::Displayable &displayable) const final;
             void setColor(const Images::Color &color) final;
     };
 }
