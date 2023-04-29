a51877cde863379842e9f797c4f06876240451a1
bahmez <younes1.bahri@epitech.eu>
2023-04-29 13:49:50 +0200

    [Refactor] (Plugins) : edit PluginLoader

--- src/Plugins/PluginLoader.hpp
+++ src/Plugins/PluginLoader.hpp
@@ -8,18 +8,33 @@
     #define PLUGINLOADER_HPP_
     #include <string>
     #include "TFactory.hpp"
+    #include <filesystem>
 
 namespace RayTracer::Plugins {
     template<typename Handler, typename Interface>
     class PluginLoader {
-    public:
-        PluginLoader(const std::string &directory);
+        public:
+            PluginLoader(const std::string &directory) : _directory(directory) { }
 
-        void load(Factories::TFactory <Handler, Interface> &factory);
+            void load(Factories::TFactory<Handler, Interface> &factory) {
+                std::string currentFilePath;
+                std::size_t sizeFilePath;
+                std::string fileName;
 
-    protected:
-        std::string _directory;
-    private:
+                factory.clearAll();
+                for (const auto &entry : std::filesystem::directory_iterator(_directory)) {
+                    currentFilePath = entry.path();
+                    sizeFilePath = currentFilePath.size();
+                    if (currentFilePath.substr(sizeFilePath - 3, sizeFilePath) == ".so") {
+                        fileName = entry.path().filename();
+                        factory.add(fileName.substr(0, sizeFilePath - 3), std::make_unique<Handler>(currentFilePath));
+                    }
+                }
+            }
+
+        protected:
+            std::string _directory;
+        private:
     };
 }
 
