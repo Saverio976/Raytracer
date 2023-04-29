83080382bacd38755fae09bea4b27e8b1c980eaa
bahmez <younes1.bahri@epitech.eu>
2023-04-29 14:54:03 +0200

    [Refactor] (Plugins) : edit PluginHandler

--- src/Plugins/PluginHandler.hpp
+++ src/Plugins/PluginHandler.hpp
@@ -8,9 +8,8 @@
 #ifndef PLUGINHANDLER_HPP_
     #define PLUGINHANDLER_HPP_
 
-    #include <string>
-    #include <memory>
-    #include <dlfcn.h>
+#include <string>
+#include <memory>
 
 class IConfig; // TODO: create an iconfig please
 
@@ -29,19 +28,9 @@ namespace RayTracer::Plugins {
              *
              * @param filePath the file path
              */
-            PluginHandler(const std::string &filePath) {
-                this->_handler = dlopen(filePath, RTLD_LAZY);
-                if (this->_handler)
-                    throw std::runtime_error("Couldn't open in PluginHandler: " + std::string(dlerror()));
-                _creator = this->getResult<Creator>("getCreator");
-                if (!_creator)
-                    throw std::runtime_error("Creator couldn't be created in PluginHandler");
-            }
+            PluginHandler(const std::string &filePath);
 
-            ~PluginHandler() {
-                this->getResult<void>("deleteCreator", this->_creator);
-                dlclose(this->_handler);
-            }
+            ~PluginHandler();
 
             /**
              * @brief Get an interface (and create it with config)
@@ -50,28 +39,9 @@ namespace RayTracer::Plugins {
              *
              * @return the interface
              */
-            std::unique_ptr<Interface> get(const IConfig &config) {
-                return this->_creator->create(config);
-            }
+            std::unique_ptr<Interface> get(const IConfig &config);
 
         protected:
-            /**
-             * @brief Call function inside the handler
-             *
-             * @param name the name of the function
-             * @param __args arguments of the function
-             *
-             * @return the result of the function called
-             */
-            template<typename T, typename... Args>
-            T getResult(const std::string &name, _Args&&... __args) {
-                void *sym = dlsym(this->_handler, name.c_str());
-
-                if (!sym)
-                    throw std::runtime_error("Error on open with dlsym in PluginHandler: " + std::string(dlerror()));
-                T (*function)(...) = reinterpret_cast<T(*)(...)>(sym);
-                return function(__args...);
-            }
             Creator *_creator;
             std::string _filePath;
             void *_handler;
