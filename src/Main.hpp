41cc4be72cc279d2c6f1c6d0e96dd88a586b7dae
Xavier Mitault <xavier.mitault@epitech.eu>
2023-05-01 17:11:55 +0200

    [Fix] (Main) : add docs

--- src/Main.hpp
+++ src/Main.hpp
@@ -16,11 +16,30 @@
 namespace RayTracer {
     class Main {
         public:
+            /**
+             * @brief Construct a new Main object from argc and argv
+             *
+             * @param argc (argc from main func)
+             * @param argv (argv from main func)
+             */
             Main(int argc, char **argv);
             ~Main() = default;
 
+            /**
+             * @brief Run the main (like the main func but in a method :eyes:)
+             */
             void run();
+            /**
+             * @brief Export the scene to ppm files
+             *
+             * @param baseFilePath
+             */
             void exportScene(const std::string &baseFilePath);
+            /**
+             * @brief Get the exit code of the main
+             *
+             * @return the exit code
+             */
             int getExitCode() const;
 
         private:
