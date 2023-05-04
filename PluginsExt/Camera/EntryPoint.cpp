fb112880c0927a5a8d5bb54edec122979d0a685e
bahmez <younes1.bahri@epitech.eu>
2023-05-04 23:37:34 +0200

    [Refactor] (Camera) : edit for handler correction

--- PluginsExt/Camera/EntryPoint.cpp
+++ PluginsExt/Camera/EntryPoint.cpp
@@ -5,6 +5,8 @@
 ** EntryPoint.cpp
 */
 
+#include <vector>
+#include "CameraEntity.hpp"
 #include "Api.hpp"
 #include "CameraCreator.hpp"
 
@@ -14,8 +16,10 @@ extern "C" {
         return new RayTracer::PluginsExt::Camera::CameraCreator();
     }
 
-    void deleteCreator(void *creator)
+    void deleteCreator(void *creator, std::vector<void *> &elements)
     {
+        for (void *element : elements)
+            delete static_cast<RayTracer::PluginsExt::Camera::CameraEntity *>(element);
         delete static_cast<RayTracer::PluginsExt::Camera::CameraCreator *>(creator);
     }
 }
