7e128af750294eb97225ffa2873b45f3a6957679
bahmez <younes1.bahri@epitech.eu>
2023-05-04 22:00:52 +0200

    [Refactor] (Sphere) : edit for the new change of IPrimitive

--- PluginsExt/Sphere/SphereEntity.hpp
+++ PluginsExt/Sphere/SphereEntity.hpp
@@ -25,7 +25,8 @@ namespace RayTracer::PluginsExt::Sphere {
             const Entities::Transform::ITransform &getTransform() const final;
             bool isCollided(const Entities::Transform::Vector3f &point) const final;
             std::optional<Entities::Transform::Vector3f> isCollided(const Images::Ray &ray) const final;
-            Images::Color getColor(const Images::Ray &ray, const Scenes::Displayable &displayable) const final;
+            Images::Color getColor(const Images::Ray &ray, const Scenes::Displayable &displayable,
+                const Entities::Transform::Vector3f &intersect) const final;
         private:
             Entities::Transform::Transform _transform;
             double _radius;
