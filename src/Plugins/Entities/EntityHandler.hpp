1889cc912b5487d8bb5eb53ca78abecc8f2217d7
Xavier Mitault <xavier.mitault@epitech.eu>
2023-05-05 22:27:40 +0000

    [Style] (Entities) : space
    Co-authored-by: bahmez <91877321+bahmez@users.noreply.github.com>

--- src/Plugins/Entities/EntityHandler.hpp
+++ src/Plugins/Entities/EntityHandler.hpp
@@ -11,7 +11,7 @@
     #include <string>
     #include "IEntity.hpp"
     #include "IEntityCreator.hpp"
-#include "ILogger.hpp"
+    #include "ILogger.hpp"
     #include "PluginHandler.hpp"
     #include "ISetting.hpp"
 
