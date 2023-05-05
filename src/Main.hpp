f8ba623ff96cae4628e715048cbd8bd7d48c915e
Xavier Mitault <xavier.mitault@epitech.eu>
2023-05-05 15:27:27 +0200

    [Refactor] (Main) : use Main class functor

--- src/Main.hpp
+++ src/Main.hpp
@@ -10,45 +10,65 @@
 
     #include "Parameters.hpp"
 #include "Scene.hpp"
+#include <exception>
 #include <memory>
 #include <string>
 
 namespace RayTracer {
     class Main {
         public:
+            class MainError : public std::exception {
+                public:
+                    explicit MainError(const std::string &message);
+                    const char *what() const noexcept override;
+                private:
+                    std::string _message;
+            };
             /**
              * @brief Construct a new Main object from argc and argv
              *
              * @param argc (argc from main func)
              * @param argv (argv from main func)
              */
-            Main(int argc, char **argv);
+            Main() = default;
             ~Main() = default;
 
-            /**
-             * @brief Run the main (like the main func but in a method :eyes:)
-             */
-            void run();
-            /**
-             * @brief Get the exit code of the main
-             *
-             * @return the exit code
-             */
-            int getExitCode() const;
-            /**
-             * @brief Print the help
-             */
-            void help() const;
+            int operator()(int argc, char **argv);
 
         private:
+            class ArgumentError : public std::exception {
+                public:
+                    explicit ArgumentError(const std::string &message);
+                    const char *what() const noexcept override;
+                private:
+                    std::string _message;
+            };
             /**
              * @brief Export the scene to ppm files
              *
              * @param baseFilePath
              */
             void exportScene(const std::string &baseFilePath);
-            Parameters &_parameters;
-            int _exitCode;
+            /**
+             * @brief Run the main (like the main func but in a method :eyes:)
+             */
+            void run();
+            /**
+             * @brief Print the help
+             */
+            void help() const;
+            /**
+             * @brief Parse the command line argumens
+             *
+             * This method can throw an ArgumentError
+             *
+             * @param argc nb of arguments
+             * @param argv arguments
+             *
+             * @return true if can proceed, false otherwise (but it is not an error)
+             */
+            bool parseCmdArgs(int argc, char **argv);
+
             std::string _baseFilePath;
             std::string _sceneConfFilePath;
             Scenes::Scene _scene;
