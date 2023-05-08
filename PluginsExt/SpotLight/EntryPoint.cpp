38b7ae09e8dd104d31094e393c91c5b6bdfac21b
bahmez <91877321+bahmez@users.noreply.github.com>
2023-05-08 22:43:54 +0200

    [Style 🩹] (SpotLight) : add 4 spaces for the cleanliness
    Co-authored-by: Xavier Mitault <xavier.mitault@epitech.eu>

--- PluginsExt/SpotLight/EntryPoint.cpp
+++ PluginsExt/SpotLight/EntryPoint.cpp
@@ -11,7 +11,7 @@
 #include "SpotLightEntity.hpp"
 
 extern "C" {
-void *getCreator(void)
+    void *getCreator(void)
     {
         return new RayTracer::PluginsExt::SpotLight::SpotLightCreator();
     }
