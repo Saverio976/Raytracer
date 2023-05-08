459d3c6e0149a10b3b17237f45acdf9f6e0fa411
bahmez <younes1.bahri@epitech.eu>
2023-05-09 01:17:03 +0200

    [Feat] (Plugins) : add MaterialLoader

---
+++ src/Plugins/Materials/MaterialLoader.hpp
@@ -0,0 +1,43 @@
+/*
+** EPITECH PROJECT, 2023
+** Raytracer
+** File description:
+** EntityLoader.hpp
+*/
+
+#ifndef ENTITYLOADER_HPP_
+    #define ENTITYLOADER_HPP_
+
+    #include <string>
+    #include "TFactory.hpp"
+    #include "IMaterial.hpp"
+    #include "PluginLoader.hpp"
+    #include "MaterialHandler.hpp"
+    #include "MaterialFactory.hpp"
+
+namespace RayTracer::Plugins::Materials {
+    /**
+     * @brief The EntityLoader
+     *
+     * class that represent an entity loader (take a directory and create EntityHandler)
+     */
+    class MaterialLoader : protected PluginLoader<MaterialHandler, RayTracer::Entities::IMaterial, Factories::MaterialFactory> {
+    public:
+        /**
+        * @brief EntityLoader constructor (doesn't load anything)
+        *
+        * @param directory the directory
+        */
+        MaterialLoader(const std::string &directory);
+        /**
+        * @brief Load all the entities in the factory singleton
+        */
+        void loadMaterials();
+
+    protected:
+    private:
+        std::string _directory;
+    };
+}
+
+#endif /*ENTITYLOADER_HPP_*/
