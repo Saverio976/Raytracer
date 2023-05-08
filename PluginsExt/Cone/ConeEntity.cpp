debefc9737150e844b38e0c35321c0836dfc20ae
SimonVermeulen <simon.vermeulen@epitech.eu>
2023-05-08 14:04:51 +0200

    [Refactor] (Cone): changed the equation of the cone

--- PluginsExt/Cone/ConeEntity.cpp
+++ PluginsExt/Cone/ConeEntity.cpp
@@ -12,7 +12,8 @@ namespace RayTracer::PluginsExt::Cone {
     ConeEntity::ConeEntity(const Scenes::ISetting &config, ILogger &logger):
         _transform(Entities::Transform::Transform(*config.get("transform"))),
         _angle(static_cast<double>(*config.get("angle"))),
-        _logger(logger)
+        _logger(logger),
+        _color(*config.get("color"))
     {
     }
 
@@ -33,39 +34,41 @@ namespace RayTracer::PluginsExt::Cone {
 
     std::optional<Entities::Transform::Vector3f> ConeEntity::isCollided(const Images::Ray &ray) const
     {
-        Entities::Transform::Vector3f coneStart = _transform.getPosition();
-        Entities::Transform::Vector3f coneDirection = _transform.getRotation();
-        const Entities::Transform::Vector3f &rayStart = ray.getOrigin();
         const Entities::Transform::Vector3f &rayDirection = ray.getDirection();
-        Entities::Transform::Vector3f deltaP(rayStart.getX() - coneStart.getX(), rayStart.getY() - coneStart.getY(), rayStart.getZ() - coneStart.getZ());
+        const Entities::Transform::Vector3f &rayOrigin = ray.getOrigin();
+        const Entities::Transform::Vector3f coneDirection = _transform.getRotation();
+        const Entities::Transform::Vector3f coneOrigin = _transform.getPosition();
+        const Entities::Transform::Vector3f rayToConeOrigin(rayOrigin.getX() - coneOrigin.getX(), rayOrigin.getY() - coneOrigin.getY(), rayOrigin.getZ() - coneOrigin.getZ());
 
-        double vDeltaPValue = rayDirection.dot(deltaP);
+        double testAngle = pow(-0.4777070063694 * _angle + 1.5, 2);
+
+        double a = pow(rayDirection.dot(coneDirection), 2) - testAngle * rayDirection.dot(rayDirection);
+        double b = 2 * (rayToConeOrigin.dot(coneDirection) * rayDirection.dot(coneDirection) - testAngle * rayToConeOrigin.dot(rayDirection));
+        double c = pow(rayToConeOrigin.dot(coneDirection), 2) - testAngle * rayToConeOrigin.dot(rayToConeOrigin);
+        double delta = (b * b) - (4 * a * c);
+
         Entities::Transform::Vector3f collision(0, 0, 0);
-
-        double a = (pow(cos(_angle), 2)) * (rayDirection.getX() * rayDirection.getX() + rayDirection.getY() * rayDirection.getY() + rayDirection.getZ() * rayDirection.getZ()) -
-                (coneDirection.getX() * coneDirection.getX() + coneDirection.getY() * coneDirection.getY() + coneDirection.getZ() * coneDirection.getZ());
-        double b = 2 * pow(cos(_angle), 2) * vDeltaPValue -
-                2 * (coneDirection.dot(rayDirection * deltaP) * coneDirection.getX() + coneDirection.dot(rayDirection * deltaP) * coneDirection.getY() + coneDirection.dot(rayDirection * deltaP) * coneDirection.getZ());
-        double c = pow(cos(_angle), 2) * (deltaP.getX() * deltaP.getX() + deltaP.getY() * deltaP.getY() + deltaP.getZ() * deltaP.getZ()) - pow(deltaP.dot(coneDirection), 2);
-        double delta = pow(b, 2) - 4 * a * c;
-
-        std::cout << _angle << std::endl;
-
         if (delta < 0) {
             return std::nullopt;
-        } else if (delta > 0) {
-            double t1 = ((-1 * b + sqrt(delta)) / (2 * a));
-            double t2 = ((-1 * b - sqrt(delta)) / (2 * a));
-            double t = t1 > t2 ? t2 : t1;
-
-            collision = rayStart + rayDirection * Entities::Transform::Vector3f(t, t, t);
         } else {
-            double t = -1 * b / (2 * a);
+            double t1 = ((-1 * b) + sqrt(delta)) / (2 * a);
+            double t2 = ((-1 * b) - sqrt(delta)) / (2 * a);
 
-            collision = rayStart + rayDirection * Entities::Transform::Vector3f(t, t, t);
+            double t = -1;
+            if (t1 > 0 && t2 > 0) {
+                t = std::min(t1, t2);
+            } else if (t1 > 0) {
+                t = t1;
+            } else if (t2 > 0) {
+                t = t2;
+            }
+            if (t > 0) {
+                collision = rayOrigin + rayDirection * Entities::Transform::Vector3f(t, t, t);
+                return collision;
+            } else {
+                return std::nullopt;
+            }
         }
-
-        return collision;
     }
 
     bool ConeEntity::isCollided(const Entities::Transform::Vector3f &point) const
@@ -75,6 +78,11 @@ namespace RayTracer::PluginsExt::Cone {
 
     Images::Color ConeEntity::getColor(const Images::Ray &ray, const Scenes::IDisplayable &displayable, const Entities::Transform::Vector3f &point) const
     {
-        return {255, 0, 0, 255};
+        return _color;
+    }
+
+    Images::Color ConeEntity::redirectionLight(const Images::Ray &ray, const Scenes::IDisplayable &displayable,
+        const Entities::Transform::Vector3f &intersect) const {
+        return {0, 0, 0, 0};
     }
 }
--- PluginsExt/Cone/ConeEntity.hpp
+++ PluginsExt/Cone/ConeEntity.hpp
@@ -22,10 +22,13 @@ namespace RayTracer::PluginsExt::Cone {
             bool isCollided(const Entities::Transform::Vector3f &point) const final;
             std::optional<Entities::Transform::Vector3f> isCollided(const Images::Ray &ray) const final;
             Images::Color getColor(const Images::Ray &ray, const Scenes::IDisplayable &displayable, const Entities::Transform::Vector3f &intersect) const final;
+            Images::Color redirectionLight(const Images::Ray &ray, const Scenes::IDisplayable &displayable,
+                const Entities::Transform::Vector3f &intersect) const final;
         private:
             Entities::Transform::Transform _transform;
             double _angle;
             ILogger &_logger;
+            Images::Color _color;
     };
 }
 
